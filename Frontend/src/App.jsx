2801eb4b2d30c41ae309455bac3def4325b62452
Santanu <santanuranjanbal@gmail.com>
2026-07-10 20:37:10 +0530

    pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import "./App.css";
+import Topbar from "./Component/Topbar/Topbar";
+import Navbar from "./Component/Navbar/Navbar";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
- <>
- 
- </>
-  )
+    <BrowserRouter>
+      <Topbar />
+
+      <Navbar />
+
+      <Routes>
+      
+
+
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
