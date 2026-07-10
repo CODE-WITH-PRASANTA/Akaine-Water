88a5a2c5a2d001a25b57f14bf6b2ba9b61862a2a
subha-prakash-rout <subhaprakashrout72@gmail.com>
2026-07-10 20:33:38 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
- <>
- 
- </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import HomeService from '../../Component/HomeService/HomeService'
+import HomeReview from '../../Component/HomeReview/HomeReview'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <HomeService/>
+      <HomeReview/>
+
+    </div>
+
   )
 }
 
