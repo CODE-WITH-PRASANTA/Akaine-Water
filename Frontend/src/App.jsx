dd843d1acf50a234e70bf62f2e13ff9eb0ea1f21
adyasa974 <adbarik60@gmail.com>
2026-07-11 09:27:37 +0530

    main

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Shop from "./Pages/Shop/Shop";
+import Contact from "./Pages/Contact/Contact";
+import Blog from "./Pages/Blog/Blog";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
- <>
- 
- </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/shop" element={<Shop/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/blog" element={<Blog/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
