7c721f270b6f1bf7c4ced0e5ac73594c29a5544f
Akashroul <roulakashkumar2003@gmail.com>
2026-07-13 10:35:57 +0530

    push

--- AdminPannel/src/App.jsx
+++ AdminPannel/src/App.jsx
@@ -12,6 +12,7 @@ import ShopPosting from "./Components/ShopPosting/ShopPosting";
 import Contact from "./Components/Contact/Contact";
 
 import "./App.css";
+import BlogManagement from "./Components/BlogManagement/BlogManagement";
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
           <Route path="/testimonial" element={<Testimonial />} />
           <Route path="/shop" element={<ShopPosting />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/blog-management" element={<BlogManagement />} />
 
         </Route>
 
