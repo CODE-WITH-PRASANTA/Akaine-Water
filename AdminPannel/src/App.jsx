c96d44bfd39eac7dcab044cbadb58a7adef9bc92
Akashroul <roulakashkumar2003@gmail.com>
2026-07-13 09:50:11 +0530

    pull

--- AdminPannel/src/App.jsx
+++ AdminPannel/src/App.jsx
@@ -1,40 +1,37 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import OurTeam from './Components/OurTeam/OurTeam';
-import Gallery from './Components/Gallery/Gallery';
-import Testiminial from './Components/Testiminial/Testiminial';
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Layout
-
-// Pages
-
-
-import "./App.css";
 import MainLayout from "./Layout/MainLayout/MainLayout";
+
+// Pages / Components
+import OurTeam from "./Components/OurTeam/OurTeam";
+import Gallery from "./Components/Gallery/Gallery";
 import Testimonial from "./pages/Testimonial/Testimonial";
 import ShopPosting from "./Components/ShopPosting/ShopPosting";
 import Contact from "./Components/Contact/Contact";
 
+import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OurTeam />} />
-        <Route path="/OurTeam" element={<OurTeam />} />
-        <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/Testiminial" element={<Testiminial/>} />
 
+        {/* Routes WITH Layout */}
         <Route element={<MainLayout />}>
-          {/* Home */}
-           <Route path="/" element={<Testimonial />} />
-           <Route path="/shop" element={<ShopPosting/>} />
-           <Route path="/contact" element={<Contact />} />
 
+          {/* Home Page */}
+          <Route path="/" element={<OurTeam />} />
+
+          <Route path="/team" element={<OurTeam />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/testimonial" element={<Testimonial />} />
+          <Route path="/shop" element={<ShopPosting />} />
+          <Route path="/contact" element={<Contact />} />
 
         </Route>
+
       </Routes>
     </BrowserRouter>
   );
