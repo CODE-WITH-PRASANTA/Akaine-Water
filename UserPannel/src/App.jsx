4ad8414781d83e90c8ebb20807e9198c0693947d
adyasa974 <adbarik60@gmail.com>
2026-07-20 13:45:42 +0530

    main

--- UserPannel/src/App.jsx
+++ UserPannel/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyOrder from "./Components/MyOrder/MyOrder";
+
 
 // Layout
 
@@ -8,8 +10,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        
-
+    <Route path="/myorder" element={<MyOrder/>} />
       </Routes>
     </BrowserRouter>
   );
